825a745b294e4ad7a7cdbf3291e550b2757fe0d9
Bogdan Ududyak <uududyak@gmail.com>
2022-11-13 22:09:01 +0200

    Added function that archiving files, but with bug

--- Online_Archiver/Controllers/HomeController.cs
+++ Online_Archiver/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ICSharpCode.SharpZipLib.Zip;
 using ICSharpCode.SharpZipLib.Core;
+using System.Text;
 
 namespace Online_Archiver.Controllers
 {
@@ -33,11 +34,62 @@ namespace Online_Archiver.Controllers
 
         public IActionResult Archiver()
         {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "UploadedFiles");
+            List<string> files = new List<string>();
+            DirectoryInfo dir = new DirectoryInfo(path);
 
-            return View();
+            files.AddRange(dir.GetFiles().Select(f => f.Name));
+
+            return View("Archiver", files);
         }
 
         [HttpPost]
+        public ActionResult Archivation(List<InputModel> files)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var filenames = files.Where(m => m.Selected == true).Select(f => f.Name).ToList();
+
+            if (!Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "ArchivedFiles")))
+                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "ArchivedFiles"));
+
+            // создаем имя для архива
+            string filename = Guid.NewGuid().ToString() + ".zip";
+            string fullZipPath = Path.Combine(Directory.GetCurrentDirectory(), "ArchivedFiles", filename);
+            // определяем потоки для создания архива
+            FileStream fsOut = System.IO.File.Create(fullZipPath);
+            ZipOutputStream zipStream = new ZipOutputStream(fsOut);
+
+            zipStream.SetLevel(7); // уровень сжатия от 0 до 9
+
+            // перебираем выбранные файлы и добавляем их в архив
+            foreach (string file in filenames)
+            {
+                FileInfo fi = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(), "UploadedFiles", file));
+
+                if (!fi.Exists)
+                    continue;
+
+                string entryName = ZipEntry.CleanName(fi.Name);
+                ZipEntry newEntry = new ZipEntry(entryName);
+                newEntry.DateTime = fi.LastWriteTime;
+                newEntry.Size = fi.Length;
+                zipStream.PutNextEntry(newEntry);
+
+                byte[] buffer = new byte[4096];
+                using (FileStream streamReader = System.IO.File.OpenRead(fi.FullName))
+                {
+                    StreamUtils.Copy(streamReader, zipStream, buffer);
+                }
+                zipStream.CloseEntry();
+            }
+
+            zipStream.IsStreamOwner = true;
+            zipStream.Close();
+
+            string file_type = "application/zip";
+            return File(fullZipPath, file_type, filename);
+        }
+
         public IActionResult Upload(IFormFile[] files)
         {
             // Iterate each files
---
+++ Online_Archiver/Models/Class.cs
@@ -0,0 +1,8 @@
+﻿namespace Online_Archiver.Models
+{
+    public class InputModel
+    {
+        public string Name { get; set; } // имя файла
+        public bool? Selected { get; set; } // выбран ли файл для загрузки
+    }
+}
